90796537ee1b8034b999ef12511af983860d2348
Sonu739657 <sonukoushal99@gmail.com>
2026-07-04 23:34:22 +0530

    program 2

---
+++ 2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/// data types
+int main(){
+    int a;
+    char c='a';
+    float f=1.2;
+    double d=1.998;
+    a=4;
+    a=9;  
+    cout <<a<<endl;
+    cout <<c<<endl;
+    cout <<f<<endl;
+    cout <<d<<endl;
+    
+}
