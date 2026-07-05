1bdb837551a6ac443e96304002055bd0d4148fb9
Sonu739657 <sonukoushal99@gmail.com>
2026-07-05 13:34:03 +0530

    progam 5

---
+++ 5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+//operatores and features
+int main(){
+    int a =3;
+    float b=3.9;
+    float c=a/b;
+    cout << c<<endl;
+
+//boolean
+    int d=2;
+    int e=3;
+    bool first = (a==b);
+    cout<<first<<endl;
+
+//logical operator
+    
+
+}
