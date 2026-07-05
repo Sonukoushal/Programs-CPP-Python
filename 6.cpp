53ea10cadc7c0132281accad7c248fd3b1802530
Sonu739657 <sonukoushal99@gmail.com>
2026-07-05 18:06:49 +0530

    program 6

---
+++ 6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"enter a value of a:"<<endl;
+    cin>>a;
+    cout<<"enter a value of b:"<<endl;
+    cin>>b;
+    cout<<"value of a= "<<a<<" "<<"value of b is "<<b<<"endl";
+
+}
