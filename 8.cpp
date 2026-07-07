deee36002dc73f06db01ddc00e4ea944aaceb394
Sonu739657 <sonukoushal99@gmail.com>
2026-07-07 06:41:12 +0530

    add more

---
+++ 8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>;
+using namespace std;
+
+int main(){
+    char ch;
+    cout<<"enter a character: "<<endl;
+    cin>>ch;
+    if (ch>='a'&&ch<='z'){
+        cout<<"this is lowercase"<<endl;
+    }
+    else{
+        if(ch>='A'&&ch<='Z'){
+            cout<<"this is uppercase"<<endl;
+        }
+        else{
+            cout<<"this is number"<<endl;
+        }
+    }
+}
