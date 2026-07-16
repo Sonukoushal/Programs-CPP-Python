23a3c6a326c6544b1b724c0d0373567ba8fe0b88
Sonu739657 <sonukoushal99@gmail.com>
2026-07-16 20:48:31 +0530

    new programs

---
+++ 16.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    while(i<=3){
+        int j=1;
+    while(j<=3){
+        cout<<4-j;
+        j+=1;
+    }
+
+    cout<<endl;
+    i+=1;}
+}
---
+++ 18.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    while(i<=6){
+        int j=1;
+        while(j<=6){
+            cout<<7-j;
+            j+=1;
+        }
+        cout<<endl;
+        i+=1;
+    }
+}
